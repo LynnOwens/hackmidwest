225c5bd454e576cc7264f39945996309722e6c06
lrowens <lrowens@naic.org>
2022-07-23 15:53:42 -0500

    add a run.sh and package.json to smooth over "it works on my machine"

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+WORKING_DIR=/repos/hackmidwest
+apt install node-js npm python3
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
 pip install -r requirements.txt
+cd twitter-client || exit
+npm install
+cd $WORKING_DIR
+cd website
+python3 main.py 146.190.42.170 80
+
