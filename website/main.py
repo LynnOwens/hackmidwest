bbe333ee3b95155538abe6a775f365a79ae68019
lrowens <lrowens@naic.org>
2022-07-23 14:20:45 -0500

    various tools

--- website/main.py
+++ website/main.py
@@ -1,12 +1,38 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template, request
 import argparse
+from typing import List
+
+from textblob import TextBlob
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app!"
+    return render_template('input_term.html')
+
+
+@app.route("/process_term", methods=['POST'])
+def process_term():
+    return request.form['term']
+
+
+def call_remote():
+    response = requests.request('GET', 'https://www.google.com')
+    print(response.content)
+
+
+def average_list(data: List[float]) -> float:
+    return sum(data) / len(data)
+
+
+def analyze_sentiment(tweets: List[dict]):
+    sentiments: List[float] = []
+    for tweet in tweets:
+        tweet_text_blob = TextBlob(tweet['text'])
+        sentiments.append(tweet_text_blob.sentiment.polarity)
+    print(str(average_list(sentiments)))
 
 
 if __name__ == "__main__":
@@ -14,4 +40,13 @@ if __name__ == "__main__":
     parser.add_argument('listenIp', help='The listen IP of the site')
     parser.add_argument('listenPort', help='The listen port of the site')
     args = parser.parse_args()
+    call_remote()
+    analyze_sentiment([
+        {
+            'text': 'sucky dude'
+        },
+        {
+            'text': 'great person'
+        }
+    ])
     app.run(host=args.listenIp, port=args.listenPort, debug=True)
