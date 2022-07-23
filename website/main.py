45407017621e24739a1840560155dbb43efccd1e
lrowens <lrowens@naic.org>
2022-07-23 13:23:57 -0500

    variable listen ip port

--- website/main.py
+++ website/main.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import argparse
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
 
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    parser = argparse.ArgumentParser(description="SBS GitLab Project Management Tool")
+    parser.add_argument('listenIp', help='The listen IP of the site')
+    parser.add_argument('listenPort', help='The listen port of the site')
+    args = parser.parse_args()
+    app.run(host=args.listenIp, port=args.listenPort, debug=True)
